6903038bb4dea6275e2784ccec34be98fe72ff36
Dani <daarro@floridauniversitaria.es>
2025-05-20 10:26:30 +0200

    Quitando rutas seguras

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
-import { RouterModule } from '@angular/router';
-import { AuthGuard } from './auth.guard';
-import { NgModule } from '@angular/core';
 import { RegisterComponent } from './views/register/register.component';
 import { WarehouseComponent } from './views/warehouse/warehouse.component';
 import { GraphicsComponent } from './views/graphics/graphics.component';
@@ -12,25 +11,18 @@ import { DataUserComponent } from './views/data-user/data-user.component';
 import { ChatIAComponent } from './views/chat-ia/chat-ia.component';
 import { DataMapComponent } from './views/data-map/data-map.component';
 
-//MODIFICAR, LAS RUTAS NO ESTAN PROTEGIDAS, AÑADIR --> canActivate: [AuthGuard] 
-export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent},
-    { path: 'login', component: LoginComponent,  },
-    { path: 'register', component: RegisterComponent},
-    { path: 'warehouse', component: WarehouseComponent },
-    { path: 'graphics', component: GraphicsComponent },
-    { path: 'product_data/:id', component: ProductDataComponent }, // se coge con el id del almacen
-    { path: 'data_user', component: DataUserComponent },
-    { path: 'data_map', component: DataMapComponent },
-    { path: 'chat_ia', component: ChatIAComponent }
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'warehouse', component: WarehouseComponent },
+  { path: 'graphics', component: GraphicsComponent },
+  { path: 'product_data/:id', component: ProductDataComponent },
+  { path: 'data_user', component: DataUserComponent },
+  { path: 'data_map', component: DataMapComponent },
+  { path: 'chat_ia', component: ChatIAComponent }
 ];
 
-/*
-@NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-    */
-  export class AppRoutingModule { }
-  
+
+export class AppRoutingModule {}
